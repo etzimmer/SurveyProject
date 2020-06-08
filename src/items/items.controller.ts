91b2b696eb3403cba8b36ef01a3df3bf9d81e61a
Ethan <ezimmerman@nccdglobal.org>
2020-06-08 09:52:36 -0500

    Updating Nest API 2

---
+++ src/items/items.controller.ts
@@ -0,0 +1,15 @@
+import { Get, Post, Controller } from '@nestjs/common';
+
+@Controller('items')
+
+export class ItemsController {
+    @Get()
+    async findAll(): Promise<string[]> {
+        return ['Pizza', 'Coke'];
+    }
+
+    @Post()
+    async create() {
+        return 'Not yet implemented';
+    }
+}
